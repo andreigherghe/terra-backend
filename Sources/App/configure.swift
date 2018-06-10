7e61b0f35146e537cf37ee0241b3ff4c6de2649a
anghergh <anghergh@student.42.fr>
2018-06-10 03:19:29 +0300

    fix linux compatibility

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -133,7 +133,7 @@ extension String {
             return results
         }
 
-        let matches = regex.matches(in: self, options: [], range: NSRange(location:0, length: self.characters.count))
+        let matches = regex.matches(in: self, options: [], range: NSRange(location:0, length: self.count))
 
         guard let match = matches.first else { return results }
 
@@ -142,10 +142,20 @@ extension String {
 
         for i in 1...lastRangeIndex {
             let capturedGroupIndex = match.range(at: i)
-            let matchedString = (self as NSString).substring(with: capturedGroupIndex)
+            let matchedString = self.substring(with: range(from: capturedGroupIndex)!)
             results.append(matchedString)
         }
 
         return results
+    }
+
+    func range(from nsRange: NSRange) -> Range<String.Index>? {
+        guard
+            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+            let to16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location + nsRange.length, limitedBy: utf16.endIndex),
+            let from = from16.samePosition(in: self),
+            let to = to16.samePosition(in: self)
+            else { return nil }
+        return from ..< to
     }
 }
